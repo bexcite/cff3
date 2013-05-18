2c20379352d3cd50128060411b34ca999f8d2b07
Roman Mazur <rmazur@stanfy.com.ua>
2013-05-18 13:52:35 +0300

    train and test

--- trainer/main.m
+++ trainer/main.m
@@ -1,68 +1,34 @@
 %% Initialization
 clear ; close all; clc
 
+columns_count = 10;
 
-fprintf('Loading Data ...\n')
-data = load('ex1data2.txt');
-X = data(:, 1:2);
-y = data(:, 3);
-m = length(y); % number of training examples
+%fprintf('Loading Data ...\n')
+[X y m] = loadData('data-input', columns_count);
 
-fprintf('First 10 examples from the dataset: \n');
-fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
-
-fprintf('\n');
-fprintf('Normalizing Features ...\n');
-
-[X mu sigma] = featureNormalize(X);
-
-X = [ones(m, 1) X];
-
-fprintf('Running gradient descent ...\n');
+%fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 0.01;
-num_iters = 400;
+num_iters = 3000;
 
 % Init Theta and Run Gradient Descent 
-theta = zeros(3, 1);
+theta = zeros(columns_count + 1, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
-fprintf('Theta computed from gradient descent: \n');
-fprintf(' %f \n', theta);
-fprintf('\n');
+%fprintf('Theta computed from gradient descent: \n');
+%fprintf(' %f \n', theta);
+%fprintf('\n');
 
-t=[1650, 3];
-price = [1, (t-mu)./sigma] * theta;
+[p, d] = predict(X, theta, y);
+computeCostMulti(X, y, theta)
 
-fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
-         ':\n $%f\n'], price);
+fprintf(['Traininig data error: ' ...
+         ':\n %f\n'], d);
 
-fprintf('Solving with normal equations...\n');
+[X y m] = loadData('data-test', columns_count);
+[p, d] = predict(X, theta, y);
+computeCostMulti(X, y, theta)
 
-data = csvread('ex1data2.txt');
-X = data(:, 1:2);
-y = data(:, 3);
-m = length(y);
-
-% Add intercept term to X
-X = [ones(m, 1) X];
-
-% Calculate the parameters from the normal equation
-theta = normalEqn(X, y);
-
-% Display normal equation's result
-fprintf('Theta computed from the normal equations: \n');
-fprintf(' %f \n', theta);
-fprintf('\n');
-
-
-t=[1650, 3];
-price = [1, t] * theta;
-
-
-% ============================================================
-
-fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
-         '(using normal equations):\n $%f\n'], price);
-
+fprintf(['Test data error: ' ...
+         ':\n %f\n'], d);
