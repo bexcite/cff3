ed895697a5bcd98486978448f6a4a2de8a4cd838
Roman Mazur <rmazur@stanfy.com.ua>
2013-05-18 14:19:17 +0300

    new changes

--- trainer/loadData.m
+++ trainer/loadData.m
@@ -1,4 +1,6 @@
-function [X, y, m] = loadData(name, columns_count)
+function [X, y, m, thetaSize] = loadData(name, columns_count)
+
+columns_count
 
 data = dlmread(name, ' ');
 X = data(:, 1:columns_count);
@@ -6,7 +8,7 @@ y = data(:, columns_count + 1);
 m = length(y); 
 
 %fprintf('First 10 examples from the dataset: \n');
-%fprintf(' x = [%.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
+%fprintf(' x = [%.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
 %fprintf('\n');
 %fprintf('Normalizing Features ...\n');
@@ -14,5 +16,8 @@ m = length(y);
 [X mu sigma] = featureNormalize(X);
 
 X = [ones(m, 1) X];
+thetaSize = columns_count + 1;
+%X = [ones(m, 1) X(:, 1) + X(:, 2) + X(:, 3)];
+%thetaSize = 2;
 
 end
--- trainer/main.m
+++ trainer/main.m
@@ -4,7 +4,7 @@ clear ; close all; clc
 columns_count = 10;
 
 %fprintf('Loading Data ...\n')
-[X y m] = loadData('data-input', columns_count);
+[X y m, thetaSize] = loadData('data-input', columns_count);
 
 %fprintf('Running gradient descent ...\n');
 
@@ -13,12 +13,9 @@ alpha = 0.01;
 num_iters = 3000;
 
 % Init Theta and Run Gradient Descent 
-theta = zeros(columns_count + 1, 1);
+theta = zeros(thetaSize, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
-
-%fprintf('Theta computed from gradient descent: \n');
-%fprintf(' %f \n', theta);
-%fprintf('\n');
+theta
 
 [p, d] = predict(X, theta, y);
 computeCostMulti(X, y, theta)
@@ -26,7 +23,7 @@ computeCostMulti(X, y, theta)
 fprintf(['Traininig data error: ' ...
          ':\n %f\n'], d);
 
-[X y m] = loadData('data-test', columns_count);
+[X y m thetaSize] = loadData('data-test', columns_count);
 [p, d] = predict(X, theta, y);
 computeCostMulti(X, y, theta)
 
