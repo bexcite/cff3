aa969d040e1bbb949646adaa70442744de9e0a22
Roman Mazur <rmazur@stanfy.com.ua>
2013-05-18 12:38:25 +0300

    Changes...

--- trainer/main.m
+++ trainer/main.m
@@ -2,9 +2,67 @@
 clear ; close all; clc
 
 
-%% ======================= Part 2: Plotting =======================
 fprintf('Loading Data ...\n')
-data = load('ex1data1.txt');
-X = data(:, 1); y = data(:, 2);
+data = load('ex1data2.txt');
+X = data(:, 1:2);
+y = data(:, 3);
 m = length(y); % number of training examples
+
+fprintf('First 10 examples from the dataset: \n');
+fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
+
+fprintf('\n');
+fprintf('Normalizing Features ...\n');
+
+[X mu sigma] = featureNormalize(X);
+
+X = [ones(m, 1) X];
+
+fprintf('Running gradient descent ...\n');
+
+% Choose some alpha value
+alpha = 0.01;
+num_iters = 400;
+
+% Init Theta and Run Gradient Descent 
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters);
+
+fprintf('Theta computed from gradient descent: \n');
+fprintf(' %f \n', theta);
+fprintf('\n');
+
+t=[1650, 3];
+price = [1, (t-mu)./sigma] * theta;
+
+fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
+         ':\n $%f\n'], price);
+
+fprintf('Solving with normal equations...\n');
+
+data = csvread('ex1data2.txt');
+X = data(:, 1:2);
+y = data(:, 3);
+m = length(y);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+% Calculate the parameters from the normal equation
+theta = normalEqn(X, y);
+
+% Display normal equation's result
+fprintf('Theta computed from the normal equations: \n');
+fprintf(' %f \n', theta);
+fprintf('\n');
+
+
+t=[1650, 3];
+price = [1, t] * theta;
+
+
+% ============================================================
+
+fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
+         '(using normal equations):\n $%f\n'], price);
 
